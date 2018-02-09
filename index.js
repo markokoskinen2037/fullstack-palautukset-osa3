0546a9c5846495c2057a5d611d8745280cfca99b
Marko Koskinen <markokoskinen2037@gmail.com>
2018-02-09 09:44:51 +0200

    3.2

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 
-let data = [
+let numeroTaulukko = [
   {name:"Reiska", numero:0451235},
   {name:"Poliisi setä", numero:112}
 ]
 
 app.get('/api/persons', (req, res) => {
-  res.json(data)
+  res.json(numeroTaulukko)
 })
+
+app.get('/info', (req, res) => {
+    res.send("<p>Taulukossa on on " + numeroTaulukko.length + " ukkelin tiedot</p>" + 
+    new Date())
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
