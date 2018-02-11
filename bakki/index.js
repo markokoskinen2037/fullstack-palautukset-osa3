f3fb9e7c829a54ccb20117ce3465dfeb71a47e91
Marko Koskinen <markokoskinen2037@gmail.com>
2018-02-11 16:43:34 +0200

    herokua

--- bakki/index.js
+++ bakki/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(morgan(':method :  :status :informaatio :res[content-length] - :response-time ms'))
 morgan.token('informaatio', function (req, res) { return JSON.stringify(req.body) })
